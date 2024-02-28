f6ef480d5862cd55e9b14a3964e21eaf6fe7d411
ameena117 <ameenajadallah@gmail.com>
2024-02-28 07:24:30 +0200

    search

--- src/Album/useAlbum.js
+++ src/Album/useAlbum.js
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 
 const useAlbum = () => {
-    const [albums, setAlbums] = useState([]);
-    const fetchData = async () => {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/albums`);
-      const data = await response.json();
-      setAlbums(data);
-    };
-  
-    useEffect(() => {
-      fetchData();
-    }, []);
+  const [albums, setAlbums] = useState([]);
+  let [matchAlbums, setMatchAlbums] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/albums`);
+    const data = await response.json();
+    setAlbums(data);
+    setMatchAlbums(data);
+  };
 
-    const fillterData = async (e) => {
-        const matchData = albums.filter(album => album.id.toString().includes(e));
-        e? setAlbums(matchData): fetchData();
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fillterData = async (e) => {
+    e ? setMatchAlbums(albums.filter(album =>
+      album.id.toString().includes(e) ||
+      album.title.toString().includes(e)
+    )) : setMatchAlbums(albums);
+  };
+
 
   return {
     matchAlbums,
@@ -25,4 +30,3 @@ const useAlbum = () => {
 };
 
 export { useAlbum };
-
--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import { Album } from "./Album";
-import { Info } from "./Info";
 
 function App() {
   return (
     <>
-      <Info/>
       <Album />
     </>
   );
